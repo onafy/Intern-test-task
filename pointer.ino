04126d22d750e40146235a671b7122e20b52da31
Nur Azizah Novitami <azizahnovitami@gmail.com>
2019-01-15 18:18:42 +0800

    input from serial monitor

--- pointer.ino
+++ pointer.ino
@@ -1,15 +1,27 @@
 #include "pointer.h"
 
-byte b; 
-uint32_t num32 = 0x12345678;
+uint32_t num32;
 uint8_t ptinputt;
  
 void setup() {
-    Serial.begin (9600); //inisialiasasi komunikasi serial
-
+Serial.begin (9600);
 }
  
 void loop() {
-    ptinputt = getPointerr(num32);
+ while (!Serial.available()); // Wait for characters
+ //get input from serial monitor
+ String data = Serial.readString();
+ Serial.println(data); 
+ 
+ //convert string to char* so it can be processed by funcstrtok
+ char* buf = strdup(data.c_str());
+ char* tok = strtok(buf, " ");
+ 
+ //convert char* to unsigned long int
+ num32 = strtoul(tok, NULL, 16);
+ ptinputt = getPointerr(num32);
+ 
  
 }
+
+ 
